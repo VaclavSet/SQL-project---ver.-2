b50ef7e927b5251413331b6cc99ba5dd7efc12db
VaclavSet <157237431+VaclavSet@users.noreply.github.com>
2024-05-05 17:21:40 +0200

    Update t_vaclav_setinek_project_sql_secondary_final.sql

--- t_vaclav_setinek_project_sql_secondary_final.sql
+++ t_vaclav_setinek_project_sql_secondary_final.sql
@@ -1,5 +1,5 @@
 CREATE TABLE t_vaclav_setinek_project_sql_secondary_final AS
-(SELECT 
+(SELECT
 	tvshs.*,
    	ROUND(
     CASE
@@ -28,28 +28,28 @@ CREATE TABLE t_vaclav_setinek_project_sql_secondary_final AS
         WHEN tvshs.country = 'Czech republic' AND tvshs.calendar_year = 2018 THEN t_2018.avg_price_food
         ELSE NULL
     END AS price_food
-FROM 
-    t_vaclav_setinek_help_second tvshs 
-LEFT JOIN 
+FROM
+    t_vaclav_setinek_help_second tvshs
+LEFT JOIN
     t_vaclav_setinek_help_first t_2009 ON tvshs.calendar_year = t_2009.calendar_year AND tvshs.calendar_year = 2009
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2010 ON tvshs.calendar_year = t_2010.calendar_year AND tvshs.calendar_year = 2010
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2011 ON tvshs.calendar_year = t_2011.calendar_year AND tvshs.calendar_year = 2011
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2012 ON tvshs.calendar_year = t_2012.calendar_year AND tvshs.calendar_year = 2012
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2013 ON tvshs.calendar_year = t_2013.calendar_year AND tvshs.calendar_year = 2013
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2014 ON tvshs.calendar_year = t_2014.calendar_year AND tvshs.calendar_year = 2014
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2015 ON tvshs.calendar_year = t_2015.calendar_year AND tvshs.calendar_year = 2015
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2016 ON tvshs.calendar_year = t_2016.calendar_year AND tvshs.calendar_year = 2016
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2017 ON tvshs.calendar_year = t_2017.calendar_year AND tvshs.calendar_year = 2017
-LEFT JOIN 
+LEFT JOIN
     t_vaclav_setinek_help_first t_2018 ON tvshs.calendar_year = t_2018.calendar_year AND tvshs.calendar_year = 2018
 GROUP BY calendar_year, country
-ORDER BY 
+ORDER BY
     calendar_year);
